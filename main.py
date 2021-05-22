6eb24ea7355d4ec98af4e91e02f611fd26107024
gobalakrishnan1996 <gobalakrishnangv@gmail.com>
2021-05-22 19:00:03 +0530

    Random

---
+++ main.py
@@ -0,0 +1,32 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+import random
+NUM_RANDOM = 50
+MIN_RANDOM = 0
+MAX_RANDOM = 1000
+def main():
+    for i in range(NUM_RANDOM):
+        num = random.randint(MIN_RANDOM, MAX_RANDOM)
+        print(num)
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    main()
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
